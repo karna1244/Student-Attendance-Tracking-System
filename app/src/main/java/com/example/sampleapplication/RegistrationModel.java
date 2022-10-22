e1f68ed4826d17ac4248eadf0d9b4d8b47e25faf
Elitecrew11 <chaitanya.swaroop129@gmail.com>
2022-10-22 17:11:00 -0500

    created getter and setter methods for each datatype

--- app/src/main/java/com/example/sampleapplication/RegistrationModel.java
+++ app/src/main/java/com/example/sampleapplication/RegistrationModel.java
@@ -8,4 +8,55 @@ public class RegistrationModel {
     String course;
     String type;
 
-    
+    public RegistrationModel() {
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getSid() {
+        return sid;
+    }
+
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+}
