d5cb5d1c10f8efa6c62501c10143b1951a589250
Elitecrew11 <chaitanya.swaroop129@gmail.com>
2022-10-22 17:40:14 -0500

    imported required libraries

--- app/src/main/java/com/example/sampleapplication/AllStudentsAttendence.java
+++ app/src/main/java/com/example/sampleapplication/AllStudentsAttendence.java
@@ -1,2 +1,10 @@
 package com.example.sampleapplication;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
