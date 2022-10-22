a588ef618ae7df9eaf5689a58de047d6e9c2a50a
Elitecrew11 <chaitanya.swaroop129@gmail.com>
2022-10-22 16:05:48 -0500

    imported libraries for instructor registration java

--- app/src/main/java/com/example/sampleapplication/InstructorRegisterActivity.java
+++ app/src/main/java/com/example/sampleapplication/InstructorRegisterActivity.java
@@ -11,3 +11,19 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.checkbox.MaterialCheckBox;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
