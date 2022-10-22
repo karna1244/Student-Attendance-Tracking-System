8fb7bac63dcdc996ada0f641acb3c2c2ac7f4f44
Elitecrew11 <chaitanya.swaroop129@gmail.com>
2022-10-22 17:12:03 -0500

    created registrationmodel method to declared that datatypes are belong to same class

--- app/src/main/java/com/example/sampleapplication/RegistrationModel.java
+++ app/src/main/java/com/example/sampleapplication/RegistrationModel.java
@@ -59,4 +59,12 @@ public class RegistrationModel {
         this.type = type;
     }
 
+    public RegistrationModel(String firstName, String lastName, String sid, String pass, String course, String type) {
+        FirstName = firstName;
+        LastName = lastName;
+        this.sid = sid;
+        this.pass = pass;
+        this.course = course;
+        this.type = type;
+    }
 }
