a021bd8e7dc31bd752f9d2ec41abb3ff099f56f9
Elitecrew11 <chaitanya.swaroop129@gmail.com>
2022-10-22 17:10:11 -0500

    created registration model class and datatypes

--- app/src/main/java/com/example/sampleapplication/RegistrationModel.java
+++ app/src/main/java/com/example/sampleapplication/RegistrationModel.java
@@ -1,1 +1,11 @@
 package com.example.sampleapplication;
+
+public class RegistrationModel {
+    String FirstName;
+    String LastName;
+    String sid;
+    String pass;
+    String course;
+    String type;
+
+    
