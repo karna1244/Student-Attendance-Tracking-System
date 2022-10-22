11d397db6d2782c4c7bd6ad35aa742a4a42f264c
Elitecrew11 <chaitanya.swaroop129@gmail.com>
2022-10-22 16:04:44 -0500

    created instructor registration java page

---
+++ app/src/main/java/com/example/sampleapplication/InstructorRegisterActivity.java
@@ -0,0 +1,13 @@
+package com.example.sampleapplication;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
