9ce7089ef3756aa2c49b0d21fce5cf912d9e8ba8
Navya Sri Alaparthi <alaparthins@outlook.com>
2022-10-21 23:07:14 -0500

    created main activity java file

---
+++ app/src/main/java/com/example/sampleapplication/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.sampleapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
