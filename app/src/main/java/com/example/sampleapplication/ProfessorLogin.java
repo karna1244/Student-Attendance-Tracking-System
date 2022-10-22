2885f0430bbc4cb1907fa760e6e131f9aa589857
Kalla <S545139@nwmissouri.edu>
2022-10-22 18:55:55 -0500

    imported libraries in professor login file

--- app/src/main/java/com/example/sampleapplication/ProfessorLogin.java
+++ app/src/main/java/com/example/sampleapplication/ProfessorLogin.java
@@ -1,2 +1,22 @@
 package com.example.sampleapplication;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
