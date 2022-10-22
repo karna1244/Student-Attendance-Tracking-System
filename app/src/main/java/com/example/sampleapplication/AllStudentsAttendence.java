ea6f571881ddf7cf19a667b076e85549a01f93eb
Elitecrew11 <chaitanya.swaroop129@gmail.com>
2022-10-22 17:42:22 -0500

    allstudentattendence class created

--- app/src/main/java/com/example/sampleapplication/AllStudentsAttendence.java
+++ app/src/main/java/com/example/sampleapplication/AllStudentsAttendence.java
@@ -8,3 +8,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+
+public class AllStudentsAttendence extends AppCompatActivity {
+
+}
